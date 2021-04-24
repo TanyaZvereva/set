544830c6aa11937779daf8a04750dcdca72ccec5
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2021-04-24 10:30:47 +0300

    eslint commit

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,27 +1,22 @@
-import {
-  Team
-}
-from '../app'
+import Team from '../app';
+
 test('Должен добавлять нового персонажа', () => {
-  const team = new Team()
-  const character = {name:'Name', type:'Bowman'}
-  expect(team.members.size).toBe(0)
-  team.add(character)
-  expect(team.members.size).toBe(1)
-  expect(() => team.add(character)).toThrowError('Такой персонаж уже существует')
-  expect(team.members.size).toBe(1)
-  // const character2 = new Character('Name', 'Bowman')
-  // expect(() => team.add(character2)).toThrowError('Такой персонаж уже существует')
-  // expect(team.members.size).toBe(2)
-})
+  const team = new Team();
+  const character = { name: 'Name', type: 'Bowman' };
+  expect(team.members.size).toBe(0);
+  team.add(character);
+  expect(team.members.size).toBe(1);
+  expect(() => team.add(character)).toThrowError('Такой персонаж уже существует');
+  expect(team.members.size).toBe(1);
+});
 test('Должен добавить несколько персонажей', () => {
-  const team = new Team()
-  team.addAll({name:'Name', type:'Bowman'}, {name:'Name1', type:'Swordsman'}, {name:'Name2', type:'Daemon'})
-  expect(team.members.size).toBe(3)
-})
+  const team = new Team();
+  team.addAll({ name: 'Name', type: 'Bowman' }, { name: 'Name1', type: 'Swordsman' }, { name: 'Name2', type: 'Daemon' });
+  expect(team.members.size).toBe(3);
+});
 test('Должен конвертировать в массив', () => {
-  const team = new Team()
-  team.addAll({name:'Name', type:'Bowman'}, {name:'Name1', type:'Swordsman'}, {name:'Name2', type:'Daemon'})
-  expect(team.members.size).toBe(3)
-  expect(team.toArray().length).toBe(3)
-})
+  const team = new Team();
+  team.addAll({ name: 'Name', type: 'Bowman' }, { name: 'Name1', type: 'Swordsman' }, { name: 'Name2', type: 'Daemon' });
+  expect(team.members.size).toBe(3);
+  expect(team.toArray().length).toBe(3);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,23 @@
-export class Team {
+export default class Team {
   constructor() {
     this.members = new Set();
   }
+
   add(character) {
     if (this.members.has(character)) {
-      throw new Error('Такой персонаж уже существует')
+      throw new Error('Такой персонаж уже существует');
     } else {
-      this.members.add(character)
+      this.members.add(character);
     }
   }
+
   addAll(...characters) {
-    for (let i = 0; i < characters.length; i++) {
-      this.members.add(characters[i])
+    for (let i = 0; i < characters.length; i += 1) {
+      this.members.add(characters[i]);
     }
   }
+
   toArray() {
-    return Array.from(this.members)
+    return Array.from(this.members);
   }
 }
-
